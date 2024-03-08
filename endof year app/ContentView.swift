1b464498a2cd77cc53003a9b4c63cf48dca34e84
PiersonSisco <096308.swcsd.us>
2024-03-08 08:07:20 -0500

    Initial Commit

---
+++ endof year app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  endof year app
+//
+//  Created by Pierson Sisco on 3/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ endof year app/endof_year_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  endof_year_appApp.swift
+//  endof year app
+//
+//  Created by Pierson Sisco on 3/8/24.
+//
+
+import SwiftUI
+
+@main
+struct endof_year_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
